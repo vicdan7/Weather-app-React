4afe08a4ffec57495ada619018691dd22d8db5e6
vicdan7 <ibarravictor334@gmail.com>
2023-04-09 14:26:50 -0500

    arreglo loading

--- src/assets/components/Loading.jsx
+++ src/assets/components/Loading.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      <h1 className='load-style'>LOADING...</h1>
+    <div id="loader">
+      <div className="content-loader">
+        <div className="loader1"></div>
+        <div className="loader2"></div>
+        <div className="loader3"></div>
+        <div className="loader4"></div>
+      </div>
+      <div className="text-load">Loading...</div>
     </div>
   );
-}
+};
 
 export default Loading;
